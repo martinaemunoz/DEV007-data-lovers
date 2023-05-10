8e8129e093e9238317fe0d834f3ca4c21ad13024
Raquel Burgos <raquelburgos13@hotmail.com>
2023-05-10 18:47:19 -0500

    estilo tarjeta y banner

--- src/data.js
+++ src/data.js
@@ -1,9 +1,24 @@
 // estas funciones son de ejemplo
 
-export const tarjetas = () => {
-  return 'example';
-};
+export const tarjetas = (data) => {
+  let tarjetaString = "";
+  for (const pokemon of data.pokemon) {
+    tarjetaString += `
+    <div class="card">
+      <div class="front tarjeta">
+        <img class="imagenPokemon" src=${pokemon.img} >
+        <p>#${pokemon.num}<br>${pokemon.name.toUpperCase()}</p> 
+    </div> 
+     <div class="back tarjeta" >
+        <p> Ataque:<br> ${pokemon.stats["base-attack"]}
+        <br>Defensa:<br> ${pokemon.stats["base-defense"]} 
+        <br>Resisencia:<br> ${pokemon.stats["base-stamina"]} 
+        <br>Max-cp:<br> ${pokemon.stats["max-cp"]}
+        <br>Max-hp:<br> ${pokemon.stats["max-hp"]}</p>
+      </div>
+    </div>`;
+    console.log(tarjetaString)
+  }
+}
+  return tarjetaString;
 
-export const anotherExample = () => {
-  return 'OMG';
-};
--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 /*import { tarjetas } from './data.js';*/
-import pokemon from './data/pokemon/pokemon.js';
+
 import data from './data/pokemon/pokemon.js';
 
 console.log(data.pokemon);
 
 
 const contenedor = document.getElementById('contenedor');
-data.pokemon.forEach(pokemones =>{
+data.pokemon.forEach(pokemon =>{
     contenedor.innerHTML += `
     <div  class="tarjeta">
-    <h2>${pokemones.name}</h2>
-    <h2>${pokemones.num}</h2>
+    <h2>${pokemon.name}</h2>
+    <h2>${pokemon.num}</h2>
+    <img src="${pokemon.img}" alt="${pokemon.name}">
     </div>
 
 
