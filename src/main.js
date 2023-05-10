9b665dfed581aa3f5d6e240a2cbeafc2400a3210
Raquel Burgos <raquelburgos13@hotmail.com>
2023-05-09 20:43:44 -0500

    primera historia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-/*import { pokemon } from './data.js';*/
-// import data from './data/lol/lol.js';
+/*import { tarjetas } from './data.js';*/
+import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-console.log("hola")
+
 console.log(data.pokemon);
+
+
+const contenedor = document.getElementById('contenedor');
+data.pokemon.forEach(pokemones =>{
+    contenedor.innerHTML += `
+    <div  class="tarjeta">
+    <h2>${pokemones.name}</h2>
+    <h2>${pokemones.num}</h2>
+    </div>
+
+
+
+    `
+
+
+})
+
+
+
+console.log()
