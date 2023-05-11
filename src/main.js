015f46f2d13d5dca89cf9be03d978b6d48216a0e
martinaemunoz <martinaemunoz@gmail.com>
2023-05-11 16:51:58 -0400

    estilos del fondo y fuente asignada

--- src/main.js
+++ src/main.js
@@ -9,18 +9,12 @@ const contenedor = document.getElementById('contenedor');
 data.pokemon.forEach(pokemon =>{
     contenedor.innerHTML += `
     <div  class="tarjeta">
+    <h2>#${pokemon.num}</h2>
     <h2>${pokemon.name}</h2>
-    <h2>${pokemon.num}</h2>
     <img src="${pokemon.img}" alt="${pokemon.name}">
     </div>
-
-
-
     `
 
 
 })
 
-
-
-console.log()
