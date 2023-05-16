669f772bea70afa200cf58cd0c7385d3952a5421
Raquel Burgos <raquelburgos13@hotmail.com>
2023-05-16 12:54:29 -0500

    cambios

--- src/data.js
+++ src/data.js
@@ -1,24 +1,20 @@
 // estas funciones son de ejemplo
 
-export const tarjetas = (data) => {
+/*export const tarjetas = (data) => {
   let tarjetaString = "";
-  for (const pokemon of data.pokemon) {
-    tarjetaString += `
-    <div class="card">
-      <div class="front tarjeta">
-        <img class="imagenPokemon" src=${pokemon.img} >
-        <p>#${pokemon.num}<br>${pokemon.name.toUpperCase()}</p> 
-    </div> 
+  for (const contenedor of data.pokemon) {
+    tarjetaString += ` 
      <div class="back tarjeta" >
-        <p> Ataque:<br> ${pokemon.stats["base-attack"]}
-        <br>Defensa:<br> ${pokemon.stats["base-defense"]} 
-        <br>Resisencia:<br> ${pokemon.stats["base-stamina"]} 
-        <br>Max-cp:<br> ${pokemon.stats["max-cp"]}
-        <br>Max-hp:<br> ${pokemon.stats["max-hp"]}</p>
+        <p> Ataque:<br> ${contenedor.stats["base-attack"]}
+        <br>Defensa:<br> ${contenedor.stats["base-defense"]} 
+        <br>Resisencia:<br> ${contenedor.stats["base-stamina"]} 
+        <br>Max-cp:<br> ${contenedor.stats["max-cp"]}
+        <br>Max-hp:<br> ${contenedor.stats["max-hp"]}</p>
       </div>
     </div>`;
-    console.log(tarjetaString)
+   
   }
-}
   return tarjetaString;
+}*/
+    
 
--- src/main.js
+++ src/main.js
@@ -2,18 +2,26 @@
 
 import data from './data/pokemon/pokemon.js';
 
-console.log(data.pokemon);
+/*console.log(data.pokemon);*/
 
 
 const contenedor = document.getElementById('contenedor');
 data.pokemon.forEach(pokemon =>{
-    contenedor.innerHTML += `
-    <div  class="tarjeta">
-    <h2>#${pokemon.num}</h2>
-    <h2>${pokemon.name}</h2>
-    <img src="${pokemon.img}" alt="${pokemon.name}">
+  contenedor.innerHTML += `
+  <div class="card">
+    <div  class="front tarjeta">
+    
+    <h2>#${pokemon.num}<br>${pokemon.name.toUpperCase()}</h2>
+    <img class="imagenPokemon" src=${pokemon.img}>
+  </div>
+    <div class="back tarjeta" >
+        <p> Ataque:<br> ${pokemon.stats["base-attack"]}
+        <br>Defensa:<br> ${pokemon.stats["base-defense"]} 
+        <br>Resisencia:<br> ${pokemon.stats["base-stamina"]} 
+        <br>Max-cp:<br> ${pokemon.stats["max-cp"]}
+        <br>Max-hp:<br> ${pokemon.stats["max-hp"]}</p>
     </div>
-    `
+  </div>`
 
 
 })
