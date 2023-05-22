bb541b1fab0e5feaa80398a4cfe40595ee6a7634
martinaemunoz <martinaemunoz@gmail.com>
2023-05-22 12:31:01 -0400

    filtrado de tipo operativo

--- src/main.js
+++ src/main.js
@@ -1,25 +1,29 @@
 import data from './data/pokemon/pokemon.js';
 import filtrarTipos from './data.js';
 
-/*console.log(data.pokemon);*/
 const pokemon = data.pokemon;
-const contenedor = document.getElementById('contenedor');
-  pokemon.forEach(pokemon =>{
-  contenedor.innerHTML += `
-  <div class="card">
-    <div class="front tarjeta">
-    <img class="imagenPokemon" src=${pokemon.img}>
-    <h2>#${pokemon.num}<br>${pokemon.name.toUpperCase()}</h2>
-  </div>
-    <div class="back tarjeta" >
-        <p> Ataque:<br> ${pokemon.stats["base-attack"]}
-        <br>Defensa:<br> ${pokemon.stats["base-defense"]} 
-        <br>Resistencia:<br> ${pokemon.stats["base-stamina"]} 
-        <br>Max-cp:<br> ${pokemon.stats["max-cp"]}
-        <br>Max-hp:<br> ${pokemon.stats["max-hp"]}</p>
-    </div>
-  </div>`
-})
+let contenedor = document.getElementById('contenedor');
+function crearTarjetas(data){
+    data.forEach(pokemon =>{
+        contenedor.innerHTML += `
+        <div class="card">
+          <div class="front tarjeta">
+          <img class="imagenPokemon" src=${pokemon.img}>
+          <h2>#${pokemon.num}<br>${pokemon.name.toUpperCase()}</h2>
+        </div>
+          <div class="back tarjeta" >
+              <p> Ataque:<br> ${pokemon.stats["base-attack"]}
+              <br>Defensa:<br> ${pokemon.stats["base-defense"]} 
+              <br>Resistencia:<br> ${pokemon.stats["base-stamina"]} 
+              <br>Max-cp:<br> ${pokemon.stats["max-cp"]}
+              <br>Max-hp:<br> ${pokemon.stats["max-hp"]}</p>
+          </div>
+        </div>`
+      })
+}
+/*console.log(data.pokemon);*/
+
+crearTarjetas(pokemon);
 
 const despliegueBoton = document.querySelectorAll('.tipoPokemon');
 /*console.log(despliegueBoton)
@@ -30,13 +34,14 @@ despliegueBoton.forEach((boton) => {
   const dropdownContent = document.getElementById(boton.dataset.target);
   //const opciones = dropdownContent.querySelectorAll('a');
   //boton.forEach((opcion) => {
-    const seleccion = boton.getAttribute('name');
-    boton.addEventListener('click', (event) => {
-      event.preventDefault();
-      //console.log(pokemon, seleccion);
-      const filtrado = filtrarTipos(pokemon, seleccion);
-      console.log(filtrado)
-    
+const seleccion = boton.getAttribute('name');
+boton.addEventListener('click', (event) => {
+    event.preventDefault();
+    //console.log(pokemon, seleccion);
+    const filtrado = filtrarTipos(pokemon, seleccion);
+    console.log(filtrado)
+    contenedor.innerHTML = "";
+    crearTarjetas(filtrado.pokemon);
       //button.textContent = opcion.textContent.toUpperCase();
     });
     /*opcion.textContent = opcion.textContent.toUpperCase();
